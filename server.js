a134968dd351b14ae7890e1734d5eebe753c0842
pavan <pavan_fw10_057@masai.school>
2021-08-23 15:25:39 +0530

    4 commit

--- server.js
+++ server.js
@@ -15,14 +15,62 @@ const connect = () => {
 }
 
 
-//products
+//products first schema
 const pdSchema = new mongoose.Schema({
     pd_name : {type: String, require: true},
-    price : Number,
+    price : {type: Number, require: true},
     colorId: [{
         type : mongoose.Schema.Types.ObjectId,
+        ref: "color",
+        require: true,
+    }],
+    genderId:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"color",
+        require:true,
     }]
+},{
+    versionKey: false,
 })
+
+const Product = mongoose.model("product",pdSchema)
+
+
+//the second one color schema,,
+const colorSchema = new mongoose.Schema({
+    colorname: {type:String, require:true}
+}, {
+    versionKey: false,
+});
+
+const color = mongoose.model("color",colorSchema)
+
+//..gender schema
+
+const genderSchema = new mongoose.Schema({
+    gender :{type:String,require:true}
+},{
+    version:false
+}
+)
+
+const Gender = mongoose.model('gender',genderSchema);
+
+
+///crud operations starting below
+
+app.post("/",async(req,res)=>{
+    try{
+        const pro = await 
+    }
+
+})
+
+
+
+
+
+
 
 const port = 4000;
 app.listen(port,async () =>{

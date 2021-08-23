9f3b5dc0cce498f0ad862419c61119662b8ee348
pavan <pavan_fw10_057@masai.school>
2021-08-23 14:41:52 +0530

    second commit

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ const app = express();
 app.use(express.json());
 
 
+const connect = () => {
+    return mongoose.connect("mongodb://127.0.0.1:27017/OlympicDb",{
+        useNewUrlParser:true,
+        useNewUrlParser:true,
+        useCreateIndex:true
+    });
+}
+
+const port = 4000;
+app.listen(port,async () =>{
+    await connect();
+    console.log("listening on port 4000")
+
+});
